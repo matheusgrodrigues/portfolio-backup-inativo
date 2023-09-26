8967224f8c1d3c5dc370b6dc465ac0c9a6fcdc25
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-09-26 17:24:31 -0300

    refactor: :art: Removendo fallback
    Este fallback não sera utilizado no contexto deste projeto, por isso, estou removendo.

--- src/components/atoms/a-avatar.tsx
+++ src/components/atoms/a-avatar.tsx
@@ -6,12 +6,10 @@ interface IAvatarRoot extends RadixAvatar.AvatarProps {
   "data-testid"?: string;
 }
 
-const AvatarRoot = styled(RadixAvatar.Root).attrs<IAvatarRoot>(
-  (props) => ({
-    as: "span",
-    "data-testid": props["data-testid"] || "a-avatar",
-  })
-)`
+const AvatarRoot = styled(RadixAvatar.Root).attrs<IAvatarRoot>((props) => ({
+  as: "span",
+  "data-testid": props["data-testid"] || "a-avatar",
+}))`
   display: inline-flex;
   align-items: center;
   justify-content: center;
@@ -28,38 +26,14 @@ interface IAvatarImage extends RadixAvatar.AvatarImageProps {
   "data-testid"?: string;
 }
 
-const AvatarImage = styled(RadixAvatar.Image).attrs<IAvatarImage>(
-  (props) => ({
-    as: "img",
-    "data-testid": props["data-testid"] || "a-avatar-image",
-  })
-)`
+const AvatarImage = styled(RadixAvatar.Image).attrs<IAvatarImage>((props) => ({
+  as: "img",
+  "data-testid": props["data-testid"] || "a-avatar-image",
+}))`
   width: 100%;
   height: 100%;
   object-fit: cover;
   border-radius: inherit;
-`;
-
-interface IAvatarFallBack extends RadixAvatar.AvatarFallbackProps {
-  "data-testid"?: string;
-}
-
-const AvatarFallback = styled(
-  RadixAvatar.Fallback
-).attrs<IAvatarFallBack>((props) => ({
-  as: "span",
-  "data-testid": props["data-testid"] || "a-avatar-fallback",
-}))`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: white;
-  color: var(--violet-11);
-  font-size: 15px;
-  line-height: 1;
-  font-weight: 500;
 `;
 
 interface IAvatar extends RadixAvatar.AvatarProps {
