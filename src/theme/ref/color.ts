9cbca8b9cb028725f6b431bd96fc1b53fc0c2b71
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-09-28 09:09:24 -0300

    feat: :sparkles: Criando refs das fontes
    Criando refs das fontes, e melhorando a nomenclatura nas cores

--- src/theme/ref/color.ts
+++ src/theme/ref/color.ts
@@ -1,14 +1,14 @@
-const por_ref_white = "#ffffff";
-const por_ref_black = "#000000";
+const por_ref_color_white = "#ffffff";
+const por_ref_color_black = "#000000";
 
 // Gray
-const por_ref_gray50 = "#f9fafb";
-const por_ref_gray500 = "#667085";
-const por_ref_gray900 = "#101828";
+const por_ref_color_gray50 = "#f9fafb";
+const por_ref_color_gray500 = "#667085";
+const por_ref_color_gray900 = "#101828";
 
 // Primary
-const por_ref_primary500 = "#9e77ed";
-const por_ref_primary600 = "#7f56d9";
+const por_ref_color_primary500 = "#9e77ed";
+const por_ref_color_primary600 = "#7f56d9";
 
 // Gradient Primary 600 -> 500 (90deg)
-const por_ref_gradient_primary_600 = `${por_ref_primary600}, linear-gradient(90deg, ${por_ref_primary600} 0%, ${por_ref_primary500} 100%));`;
+const por_ref_color_gradient_primary600 = `${por_ref_color_primary600}, linear-gradient(90deg, ${por_ref_color_primary600} 0%, ${por_ref_color_primary500} 100%));`;
---
+++ src/theme/ref/font.ts
@@ -0,0 +1,15 @@
+// Family
+const por_ref_font_family = "Inter";
+
+// Weight
+const por_ref_font_weight_regular = "400";
+const por_ref_font_weight_medium = "500";
+const por_ref_font_weight_semibold = "600";
+
+// Size: Text
+const por_ref_font_size_textSm = "14px";
+const por_ref_font_size_textMd = "16px";
+const por_ref_font_size_textXl = "20px";
+
+// Size: Display
+const por_ref_font_size_displayLg = "48px";
