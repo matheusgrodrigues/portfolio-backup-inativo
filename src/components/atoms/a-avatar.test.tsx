b305d933bbd7262875d6f957fa673be011783f7b
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-09-28 20:43:23 -0300

    test: :art: Melhorando o teste do a-avatar
    Deixando o teste menor e mais objetivo

--- src/components/atoms/a-avatar.test.tsx
+++ src/components/atoms/a-avatar.test.tsx
@@ -8,7 +8,7 @@ import { Avatar } from "./a-avatar";
 describe("Deve renderizar o Avatar, corretamente", () => {
   // ==================================================================
 
-  it("Deve renderizar o Avatar com  a prop, src.", () => {
+  it("Deve renderizar o Avatar com  a prop, src e alt.", () => {
     // Arrange
     const image_src =
       "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80";
@@ -24,25 +24,6 @@ describe("Deve renderizar o Avatar, corretamente", () => {
     expect(get_avatar).toBeInTheDocument();
     expect(get_avatar_image).toBeInTheDocument();
     expect(get_avatar_image.getAttribute("src")).not.toHaveLength(0);
-  });
-
-  // ==================================================================
-
-  it("Deve renderizar o Avatar com a prop, alt.", () => {
-    // Arrange
-    const image_src =
-      "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80";
-    const image_alt = "Matheus";
-
-    render(<Avatar src={image_src} alt={image_alt} />);
-
-    // Act
-    const get_avatar = screen.getByTestId("a-avatar");
-    const get_avatar_image = screen.getByTestId("a-avatar-image");
-
-    // Assert
-    expect(get_avatar).toBeInTheDocument();
-    expect(get_avatar_image).toBeInTheDocument();
-    expect(get_avatar_image.getAttribute("alt")).not.toHaveLength(0);
+    expect(get_avatar_image).toHaveProperty("alt");
   });
 });
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,18 +1,12 @@
-import { ReactNode, useContext } from "react";
-
-import { ThemeContext } from "styled-components";
+import { ReactNode } from "react";
 
 import { Avatar } from "../../atoms";
 
 export default function PageHome(): ReactNode {
-
-
-
-
   return (
     <>
       <Avatar
-        src=""
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
         alt=""
       />
     </>
