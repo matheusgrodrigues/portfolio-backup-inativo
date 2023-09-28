5756e4efa19aef6db51188ea250260175cab605c
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2023-09-28 11:11:43 -0300

    perf: :zap: Ajustando exibicao dos components nos tests
    Agora os testes estao utilizando o helpers e não o original do jhest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import "./globals.css";
 import type { Metadata } from "next";
+
+// Reset css
+import "./globals.css";
 
 // Font
 import { Inter } from "next/font/google";
--- src/components/atoms/a-avatar.test.tsx
+++ src/components/atoms/a-avatar.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@/src/helpers/test/rtl-render";
+
 import "@testing-library/jest-dom";
 
 // Atom
